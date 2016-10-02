b8b5a3bc999da69ea7fbc1ce2c363c7da2391623
Vili Kinnunen <vili.kinnunen1@gmail.com>
2016-10-02 10:01:36 +0300

    Small fixes

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 /**
- * Created by vilik on 1.10.2016.
+ * Created by vilik on 2.10.2016.
  */
-
 module.exports = {
     port: 80,
     db_host: "localhost",
     db_user: "username",
     db_password: "password",
     db_name: "database",
-    token_secret: "secret-token"
+    token_secret: "secret"
 };
