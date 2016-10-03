e301989dc621d2ab61aa0c56dbadf4de52468cda
Vili Kinnunen <vili.kinnunen1@gmail.com>
2016-10-03 16:15:30 +0300

    Cleaning up, correcting some jslint errors

--- authentication.js
+++ authentication.js
@@ -1,116 +1,114 @@
 /**
  * Created by vilik on 1.10.2016.
  */
-var config = require("./config"),
-    jwt = require("jwt-simple"),
-    bcrypt = require("bcrypt-nodejs"),
-    db = require("./db-connector");
-
-function expiresIn(numDays) {
+(function () {
     "use strict";
-    var dateObj = new Date();
-    return dateObj.setDate(dateObj.getDate() + numDays);
-}
+    var config = require("./config"),
+        jwt = require("jwt-simple"),
+        bcrypt = require("bcrypt-nodejs"),
+        db = require("./db-connector");
 
-var auth = {
-    register: function (username, password, callback) {
-        "use strict";
-        var usernameRegex = /^[A-Za-z0-9_\-]{3,20}$/,
-            passwordRegex = /^[^\s]{8,50}$/,
-            usernameValid = username.match(usernameRegex),
-            passwordValid = password.match(passwordRegex);
+    function expiresIn(numDays) {
+        var dateObj = new Date();
+        return dateObj.setDate(dateObj.getDate() + numDays);
+    }
 
-        if (usernameValid && passwordValid) {
-            bcrypt.hash(password, null, null, function (err, hash) {
+    module.exports = {
+        register: function (username, password, callback) {
+            var usernameRegex = /^[A-Za-z0-9_\-]{3,20}$/,
+                passwordRegex = /^[^\s]{8,50}$/,
+                usernameValid = username.match(usernameRegex),
+                passwordValid = password.match(passwordRegex);
+
+            if (usernameValid && passwordValid) {
+                bcrypt.hash(password, null, null, function (err, hash) {
+                    if (!err) {
+                        var user = {username: username, password: hash};
+                        db.query("INSERT INTO User SET ?", user, function (err, result) {
+                            if (!err) {
+                                callback(null, result.insertId);
+                            } else {
+                                callback({
+                                    code: 500,
+                                    message: err.message // HIDE
+                                });
+                            }
+                        });
+                    } else {
+                        callback({
+                            code: 500,
+                            message: err.message // HIDE
+                        });
+                    }
+                });
+            } else {
+                callback({
+                    code: 400,
+                    message: "Username and/or password didn't meet requirements"
+                });
+            }
+        },
+
+        login: function (username, password, callback) {
+            db.query("SELECT * FROM User WHERE username = ?", username, function (err, rows) {
                 if (!err) {
-                    var user = {username: username, password: hash};
-                    db.query("INSERT INTO User SET ?", user, function (err, result) {
-                        if (!err) {
-                            callback(null, result.insertId);
-                        } else {
-                            callback({
-                                code: 500,
-                                message: err.message // HIDE
-                            });
-                        }
-                    });
+                    if (rows.length === 1) {
+                        bcrypt.compare(password, rows[0].password, function (err, res) {
+                            if (!err) {
+                                if (res) {
+                                    var expires = expiresIn(14);
+                                    var token = jwt.encode({
+                                        user: rows[0].id,
+                                        exp: expires
+                                    }, config.token_secret);
+                                    callback(null, token);
+                                } else {
+                                    callback({
+                                        code: 401,
+                                        message: "Invalid credentials"
+                                    });
+                                }
+                            } else {
+                                callback({
+                                    code: 500,
+                                    message: err.message // HIDE
+                                });
+                            }
+                        });
+                    } else {
+                        callback({
+                            code: 401,
+                            message: "Invalid credentials" // HIDE
+                        });
+                    }
                 } else {
                     callback({
                         code: 500,
-                        message: err.message // HIDE
+                        message: err.message
                     });
                 }
             });
-        } else {
-            callback({
-                code: 400,
-                message: "Username and/or password didn't meet requirements"
-            });
-        }
-    },
+        },
 
-    login: function (username, password, callback) {
-        "use strict";
-        db.query("SELECT * FROM User WHERE username = ?", username, function (err, rows) {
-            if (!err) {
-                if (rows.length === 1) {
-                    bcrypt.compare(password, rows[0].password, function (err, res) {
-                        if (!err) {
-                            if (res) {
-                                var expires = expiresIn(14);
-                                var token = jwt.encode({
-                                    user: rows[0].id,
-                                    exp: expires
-                                }, config.token_secret);
-                                callback(null, token);
-                            } else {
-                                callback({
-                                    code: 401,
-                                    message: "Invalid credentials"
-                                });
-                            }
-                        } else {
-                            callback({
-                                code: 500,
-                                message: err.message // HIDE
-                            });
-                        }
-                    });
+        validate: function (token) {
+            try {
+                var decoded = jwt.decode(token, config.token_secret);
+
+                if (decoded.exp <= Date.now()) {
+                    return {
+                        user: decoded.user,
+                        expired: true
+                    };
                 } else {
-                    callback({
-                        code: 401,
-                        message: "Invalid credentials" // HIDE
-                    });
+                    return {
+                        user: decoded.user,
+                        expired: false
+                    };
                 }
-            } else {
-                callback({
-                    code: 500,
-                    message: err.message
-                });
+            } catch (err) {
+                console.log(err.message);
+                return false;
             }
-        });
-    },
-
-    validate: function (token) {
-        "use strict";
-        try {
-            var decoded = jwt.decode(token, config.token_secret);
-
-            if (decoded.exp <= Date.now()) {
-                return {
-                    user: decoded.user,
-                    expired: true
-                };
-            } else {
-                return {
-                    user: decoded.user,
-                    expired: false
-                };
-            }
-        } catch (err) {
-            return false;
         }
-    }
-};
-
-module.exports = auth;
+    };
+}());
